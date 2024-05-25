8854a01fe9ab258014d8cdf5e74116b758dc1a64
Filippov Konstantin <fkv.katafey@yandex.ru>
2024-05-26 01:01:48 +0400

    Выполнил ТЗ по 3-му спринту

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -2,18 +2,63 @@
 //  ViewController.swift
 //  Counter
 //
-//  Created by Константин Филиппов on 26.05.2024.
+//  Created by Константин Филиппов on 24.05.2024.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var counter: Int = 0 {
+        didSet {
+            updateCounterValve()
+        }
+    }
+    var date: String {return DateFormatter.localizedString(from: Date(), dateStyle: .short, timeStyle: .medium)}
+    
+    @IBOutlet weak var counterValve: UILabel!
+    @IBOutlet weak var changeValve: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        counterValve.text = ("\(counter)")
+        changeValve.text = ("История изменений:\n")
+        changeValve.isEditable = false
+        
+    }
+    
+    @IBAction func counterPlus(_ sender: Any) {
+        counter += 1
+        changeValve.text += ("[\(date)]: значение изменено на +1\n")
+        print("\(date) Значение счетчика: \(counter)")
     }
 
-
+    @IBAction func counterMinus(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+            changeValve.text += ("[\(date)]: значение изменено на -1\n")
+            print("\(date) Значение счетчика: \(counter)")
+        } else {
+            changeValve.text += ("[\(date)]: попытка уменшить значение счетчика ниже 0\n")
+            print("\(date) Попытка уменшить значение счетчика ниже 0")
+        }
+        
+    }
+    
+    @IBAction func counterClean(_ sender: Any) {
+        counter = 0
+        changeValve.text += ("[\(date)]: значение сброшено\n")
+        print("\(date) Значение сброшено")
+    }
+    
+    @IBAction func consolClean(_ sender: Any) {
+        changeValve.text = ("История изменений:\n")
+    }
+    
+    private func updateCounterValve(){
+        counterValve.text = ("\(counter)")
+    }
+        
 }
 
--- CounterTests/CounterTests.swift
+++ CounterTests/CounterTests.swift
@@ -2,7 +2,7 @@
 //  CounterTests.swift
 //  CounterTests
 //
-//  Created by Константин Филиппов on 26.05.2024.
+//  Created by Константин Филиппов on 24.05.2024.
 //
 
 import XCTest
